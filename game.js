66cc6df17ce2ffb5d791e116c30aed5f3538440b
Regan Losey <regan.losey@gmail.com>
2021-09-27 11:26:44 -0700

    Add zombie win conditions and check for winner function

--- game.js
+++ game.js
@@ -6,23 +6,17 @@ class Game {
     this.devilTurn = false;
     this.turnsTaken = 0;
     this.currentPlayer = this.zombie;
-    this.cells = [
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-    ];
+    this.cells = ['', '', '', '', '', '', '', '', ''];
+    // this.winningCombos = [
+    //   [1, 2, 3], [4, 5, 6], [7,8,9],
+    //   [1,4,7], [2, 5, 8], [3, 6, 9],
+    //   [3,5,7], [1,5,9]
+    // ]
   }
-
-    updateCurrentCell(cellId){
-      var currentCell = parseInt(cellId);
-      this.cells[currentCell] = this.currentPlayer.token;
-    }
+  updateCurrentCell(cellId) {
+    var currentCell = cellId;
+    this.cells[currentCell] = this.currentPlayer.token;
+  }
 
   newTurn() {
     if (this.zombieTurn === true) {
@@ -36,10 +30,29 @@ class Game {
     }
   }
 
-  // evaluateCurrentCellToken() {
-  //   for (var i = 0; i < this.cells.length; i++) {
-  //     if (this.cells[i] === this.zombie.token) {
-  //       this.cells[i] = 'ztoken'
-  //     } else if (this.cells[i] === this.devil.token)
+  winningCombosZombie() {
+    if (this.cells[1] === this.zombie.token && this.cells[2] === this.zombie.token && this.cells[3] === this.zombie.token ||
+      this.cells[4] === this.zombie.token && this.cells[5] === this.zombie.token && this.cells[6] === this.zombie.token ||
+      this.cells[7] === this.zombie.token && this.cells[8] === this.zombie.token && this.cells[9] === this.zombie.token ||
+      this.cells[1] === this.zombie.token && this.cells[4] === this.zombie.token && this.cells[7] === this.zombie.token ||
+      this.cells[2] === this.zombie.token && this.cells[5] === this.zombie.token && this.cells[8] === this.zombie.token ||
+      this.cells[3] === this.zombie.token && this.cells[6] === this.zombie.token && this.cells[9] === this.zombie.token ||
+      this.cells[3] === this.zombie.token && this.cells[5] === this.zombie.token && this.cells[7] === this.zombie.token ||
+      this.cells[1] === this.zombie.token && this.cells[5] === this.zombie.token && this.cells[9] === this.zombie.token
+    ) {
+      return true;
+    } else {
+      return false
+    }
+  }
 
+
+  declareWinner() {
+    if (this.winningCombosZombie()) {
+      currentTurn.innerHTML = `<h1>${this.zombie.name} Wins.</h1>
+      <button class="reset-button"id="reset-btn">Play Again?
+      </button>`
+      this.zombie.wins += 1;
+    }
+  }
 }
