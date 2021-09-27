dc785cec682a590d842ab3ff1fd4baa4d3c30b4f
Regan Losey <regan.losey@gmail.com>
2021-09-26 22:09:54 -0700

    Condense place token function

--- main.js
+++ main.js
@@ -15,36 +15,32 @@ var newGame = new Game;
 // Event Handlers
 function resetGame(event) {
   event.preventDefault;
-  gameBoard.innerHTML = "";
-  gameBoard.innerHTML += `
-    <div class="cell cell1" id="cell1"></div>
-    <div class="cell cell2" id="cell2"></div>
-    <div class="cell cell3" id="cell3"></div>
-    <div class="cell cell4" id="cell4"></div>
-    <div class="cell cell5" id="cell5"></div>
-    <div class="cell cell6" id="cell6"></div>
-    <div class="cell cell7" id="cell7"></div>
-    <div class="cell cell8" id="cell8"></div>
-    <div class="cell cell9" id="cell9"></div>`
+  // gameBoard.innerHTML = "";
+  // gameBoard.innerHTML += `
+  //   <div class="cell cell1" id="cell1"></div>
+  //   <div class="cell cell2" id="cell2"></div>
+  //   <div class="cell cell3" id="cell3"></div>
+  //   <div class="cell cell4" id="cell4"></div>
+  //   <div class="cell cell5" id="cell5"></div>
+  //   <div class="cell cell6" id="cell6"></div>
+  //   <div class="cell cell7" id="cell7"></div>
+  //   <div class="cell cell8" id="cell8"></div>
+  //   <div class="cell cell9" id="cell9"></div>`
   newGame.zombieTurn = true;
   showCurrentPlayer();
 }
 
 function placeToken(event) {
-  if (!event.target.innerHTML) {
+  var selectedCell = event.target;
+  var cellId = event.target.id;
+  if (!selectedCell.innerHTML) {
     event.target.innerHTML = `${newGame.currentPlayer.token}`;
-    newGame.turnsTaken++
+    newGame.updateCurrentCell(cellId);
+    newGame.turnsTaken += 1
     newGame.newTurn();
     showCurrentPlayer();
     return;
-  // } else if (event.target.closest('.cell') && newGame.devilTurn === true) {
-  //   event.target.innerHTML = `${newGame.currentPlayer.token}`;
-  //   newGame.turnsTaken++
-  //   newGame.newTurn();
-  //   showCurrentPlayer();
-  //   return;
-  // }
-}
+  }
 }
 
 
