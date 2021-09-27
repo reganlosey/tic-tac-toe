5b54f9d48e28787a8214c926076fb32310ba8056
Regan Losey <regan.losey@gmail.com>
2021-09-26 22:10:41 -0700

    Add method to track current token position

--- game.js
+++ game.js
@@ -7,22 +7,22 @@ class Game {
     this.turnsTaken = 0;
     this.currentPlayer = this.zombie;
     this.cells = [
-      'one',
-      'two',
-      'three',
-      'four',
-      'five',
-      'six',
-      'seven',
-      'eight',
-      'nine'
-    ]
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+    ];
   }
 
-  // updateCurrentCell(cell){
-  //   var numberedCell = parseInt(cell);
-  //   this.cells[numberedCell] = this.currentPlayer.token
-  // }
+    updateCurrentCell(cellId){
+      var currentCell = parseInt(cellId);
+      this.cells[currentCell] = this.currentPlayer.token;
+    }
 
   newTurn() {
     if (this.zombieTurn === true) {
@@ -35,21 +35,11 @@ class Game {
       this.currentPlayer = this.zombie;
     }
   }
-//0=1
-//1=2
-//2=3
-//3=4
-//4=5
-//5=6
-//6=7
-//7=8
-//8=9
-  // verticalWin(cells, emoji){
-  //   if (cells[0] === emoji && cells[3] === emoji && cells[4] === emoji ||
-  //       cells )
-  // }
-  // }
+
+  // evaluateCurrentCellToken() {
+  //   for (var i = 0; i < this.cells.length; i++) {
+  //     if (this.cells[i] === this.zombie.token) {
+  //       this.cells[i] = 'ztoken'
+  //     } else if (this.cells[i] === this.devil.token)
 
 }
-// object for messages to display? this.zombie.winner = "this.zombie.name wins!"
-//same for turns. this.zombie.turn = It's this.zombie.name's turn!
